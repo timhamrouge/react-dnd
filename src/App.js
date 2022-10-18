07f23be884725f71706415e688bcfc4252922cde
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2022-10-18 17:20:28 +0100

    trying top get it working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { useDrag } from 'react-dnd'
+import { ItemTypes } from './Constants'
+
+/**
+ * Your Component
+ */
+function Card({ isDragging, text }) {
+  const [{ opacity }, dragRef] = useDrag(
+    () => ({
+      type: ItemTypes.CARD,
+      item: { text },
+      collect: (monitor) => ({
+        opacity: monitor.isDragging() ? 0.5 : 1
+      })
+    }),
+    []
+  )
+  return (
+    <div ref={dragRef} style={{ opacity }}>
+      {text}
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Card isDragging={true} text="hello world"/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +42,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
